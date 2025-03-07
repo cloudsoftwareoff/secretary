70a34ec8c0af42e9075e7e5c47ed14ab6db0ac88
cloudsoftwareoff <chahid.ahmadi2@gmail.com>
2025-03-07 22:50:33 +0100

    v2.2+1

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme(),
-      home: WatermarkWrapper(
-        child: Wrapper(),
-      ),
+      home: Wrapper(),
     );
   }
 }
